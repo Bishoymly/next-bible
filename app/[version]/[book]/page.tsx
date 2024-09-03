9fe4963a2b3e0a912c6497db0a6627ab17398795
Bishoy Labib <bishoy.labib@integrant.com>
2024-09-03 14:49:26 -0700

    added book intro AI curation

--- app/[version]/[book]/page.tsx
+++ app/[version]/[book]/page.tsx
@@ -1,5 +1,15 @@
+"use server";
 import { BibleBookHome } from "@/components/bible-book-home";
+import curateBook from "@/lib/curateBook";
+import fs from "fs";
+import path from "path";
 
-export default function BookPage() {
-  return <BibleBookHome />;
+export default async function BookPage({ params }) {
+  const book = params.book;
+  const filePath = path.join(process.cwd(), "public", "data", "key_english.json");
+  const fileContents = fs.readFileSync(filePath, "utf-8");
+  const books = JSON.parse(fileContents);
+  const bookInfo = books.resultset.keys.filter((b) => b.n.toLowerCase() === book.replace(/-/g, " "))[0];
+  const curation = await curateBook(book);
+  return <BibleBookHome book={book} curation={curation} bookInfo={bookInfo} />;
 }
---
+++ lib/curateBook.ts
@@ -0,0 +1,28 @@
+"Use Server";
+import { openai } from "@ai-sdk/openai";
+import { generateObject } from "ai";
+import { kv } from "@vercel/kv";
+import { z } from "zod";
+
+export default async function curateBook(book) {
+  const key = book;
+
+  // Check if we have a cached response
+  const cached = await kv.get(key);
+  if (cached != null && cached.overviewParagraphs) {
+    return cached;
+  }
+
+  const { object } = await generateObject({
+    model: openai("gpt-4o-mini"),
+    schema: z.object({
+      overviewParagraphs: z.array(z.string()),
+      sections: z.array(z.object({ title: z.string(), startChapter: z.number() })),
+      sketchPrompt: z.string(),
+    }),
+    prompt: `As a reformed baptist scholar talking to an average bible student. Give me an introduction to the book of ${book} that will help me understand the book, the writer, the setting and how to split its chapters into useful sections. Don't included chapter numbers in section titles. Also please give me a prompt that will generate a drawing sketch that represents this book.`,
+  });
+
+  await kv.set(key, object);
+  return object;
+}
