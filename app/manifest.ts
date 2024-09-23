62fcb81b830d57a80d9790edb699afa08f6fffc2
Bishoy Labib <bishoy.labib@integrant.com>
2024-09-23 14:11:22 -0700

    PWA icons

--- app/manifest.ts
+++ app/manifest.ts
@@ -11,14 +11,28 @@ export default function manifest(): MetadataRoute.Manifest {
     theme_color: "#000000",
     icons: [
       {
-        src: "/favicon.png",
+        src: "icons/manifest-icon-192.maskable.png",
         sizes: "192x192",
         type: "image/png",
+        purpose: "any",
       },
       {
-        src: "/favicon.png",
+        src: "icons/manifest-icon-192.maskable.png",
+        sizes: "192x192",
+        type: "image/png",
+        purpose: "maskable",
+      },
+      {
+        src: "icons/manifest-icon-512.maskable.png",
         sizes: "512x512",
         type: "image/png",
+        purpose: "any",
+      },
+      {
+        src: "icons/manifest-icon-512.maskable.png",
+        sizes: "512x512",
+        type: "image/png",
+        purpose: "maskable",
       },
     ],
   };
